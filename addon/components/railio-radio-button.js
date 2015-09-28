c55bff9ee75bdfde47be8bdb25061021bacc073b
Chantal Broeren <chantal.broeren@fabriquartz.com>
2015-09-28 11:27:33 +0200

    add new component: radio-select

---
+++ addon/components/railio-radio-button.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+import layout from 'ember-railio-form-components/templates/components/railio-radio-button';
+
+const { computed } = Ember;
+
+export default Ember.Component.extend({
+  layout: layout,
+  classNames: ['radio-select__option'],
+  classNameBindings: ['checked:radio-select__option--selected:radio-select__option--unselected'],
+
+  checked: computed('option', 'selection', function() {
+    const optionValuePath = this.get('optionValuePath');
+
+    if (optionValuePath) {
+      return this.get('option.' + optionValuePath) === this.get('selection');
+    }
+
+    return Ember.isEqual(
+      this.get('option'),
+      this.get('selection')
+    );
+  }),
+
+  label: computed('option', 'optionLabelPath', function() {
+    const optionLabelPath = this.get('optionLabelPath');
+
+    if (optionLabelPath) {
+      return this.get('option.' + optionLabelPath);
+    }
+    return this.get('option');
+  }),
+
+  click: function() {
+    const optionValuePath = this.get('optionValuePath');
+    let value = this.get('option');
+
+    if (optionValuePath) {
+      value = this.get('option.' + optionValuePath);
+    }
+
+    if (typeof this.attrs.updated === 'function') {
+      this.attrs.updated(value);
+    }
+  }
+});
--- tests/dummy/app/components/test-component.js
+++ tests/dummy/app/components/test-component.js
@@ -3,8 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   object: Ember.Object.create({
     number: -3.4,
-    text: 'test text'
+    text: 'test text',
+    radioValue: ''
   }),
+
+  optionList: ['OPTION 1', 'OPTION 2', 'OPTION 3'],
 
   actions: {
     update(object, propertyPath, value) {
