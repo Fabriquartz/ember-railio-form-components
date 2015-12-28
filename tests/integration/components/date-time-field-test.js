776dedec1a61b74db9a7ea6e28f9d7d359774c38
Chantal Broeren <chantal.broeren@fabriquartz.com>
2015-12-28 21:38:56 +0100

    Date-time-field-with-html-inputs

--- tests/integration/components/date-time-field-test.js
+++ tests/integration/components/date-time-field-test.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 import { moduleForComponent, test } from 'ember-qunit';
 
-const { run } = Ember;
+const { get, set, run } = Ember;
 
 moduleForComponent('date-time-field', 'Integration | Component | {{date-time-field}}', {
 
@@ -14,24 +14,49 @@ moduleForComponent('date-time-field', 'Integration | Component | {{date-time-fie
   }
 });
 
-test('shows date and time', function(assert) {
-  this.set('value', new Date(2015, 0, 1, 12, 30));
-  this.render(hbs`{{date-time-field value=value updated=(action "update")}}`);
+test('Has date input and time input', function(assert) {
+  this.render(hbs`{{date-time-field}}`);
+  assert.equal(this.$('input').length, 2,
+              'There should be 2 input elements.');
+});
 
-  const $dateInput = this.$('.date-picker');
-  const $timeInput = this.$('.time-field');
+test('Renders date and time in inputs', function(assert) {
+  set(this, 'datetime', new Date(2015, 0, 1, 12, 30));
 
-  assert.equal($dateInput.val(), '01-01-15', 'shows date');
-  assert.equal($timeInput.val(), '12:30', 'shows time');
+  this.render(hbs`{{date-time-field value=datetime}}`);
 
-  run(() => {
-    $dateInput.val('02-01-15');
-    $dateInput.trigger('input');
-    $timeInput.val('13:30');
-    $timeInput.trigger('input');
+  assert.equal(this.$('input[type="date"]:eq(0)').val(), '2015-01-01');
+  assert.equal(this.$('input[type="time"]:eq(0)').val(), '12:30');
+});
+
+test('Changes value after changing input', function(assert) {
+  set(this, 'datetime', new Date(2015, 0, 1, 12, 30));
+
+  this.on('update', function(value) {
+    set(this, 'datetime', value);
   });
 
-  assert.equal(+this.get('value'), +(new Date(2015, 0, 2, 13, 30)), 'updates date and time');
+  this.render(hbs`{{date-time-field value=datetime updated=(action 'update')}}`);
+
+  const $dateInput = this.$('input[type="date"]:eq(0)');
+
+  run(() => {
+    $dateInput.val('2015-01-02');
+    $dateInput.trigger('change');
+  });
+
+  assert.equal(+get(this, 'datetime'), +(new Date(2015, 0, 2, 12, 30)),
+               'updates date');
+
+  const $timeInput = this.$('input[type="time"]:eq(0)');
+
+  run(() => {
+    $timeInput.val('13:13');
+    $timeInput.trigger('change');
+  });
+
+  assert.equal(+get(this, 'datetime'), +(new Date(2015, 0, 2, 13, 13)),
+               'updates time');
 });
 
 test('set to now', function(assert) {
