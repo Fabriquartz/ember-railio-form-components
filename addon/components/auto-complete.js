71966251ff5e833cdbd0dba252b401d188dbf319
Chantal Broeren <cbroeren@users.noreply.github.com>
2017-07-04 14:51:23 +0200

    fix auto-complete grouping (#40)

--- addon/components/auto-complete.js
+++ addon/components/auto-complete.js
@@ -1,10 +1,22 @@
-import Component       from 'ember-component';
-import layout          from '../templates/components/auto-complete';
-import computedGroupBy from '../utils/computed/group-by';
+import Ember     from 'ember';
+import Component from 'ember-component';
+import layout    from '../templates/components/auto-complete';
+import groupBy   from '../utils/group-by';
+import computed  from 'ember-computed';
+import get       from 'ember-metal/get';
+
+const { defineProperty } = Ember;
 
 export default Component.extend({
   classNames: ['auto-complete'],
   layout: layout,
 
-  groupedContent: computedGroupBy('content', 'groupLabelPath')
+  didReceiveAttrs() {
+    let groupLabelPath = get(this, 'groupLabelPath');
+
+    defineProperty(this, 'groupedContent',
+    computed(`content.@each.{${groupLabelPath}}`, function() {
+      return groupBy(get(this, 'content'), groupLabelPath);
+    }));
+  }
 });
