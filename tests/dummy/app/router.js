174bee9b6d38e01ccca151083657a4e94e7c6cd5
Arne Diehl <arne.diehl@fabriquartz.com>
2017-12-11 15:45:47 +0100

    fix errors

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,7 +1,7 @@
 import EmberRouter from '@ember/routing/router';
 import config      from './config/environment';
 
-export default Router.extend({
+export default EmberRouter.extend({
   location: config.locationType,
   rootURL:  config.rootURL
 }).map(function() {});
