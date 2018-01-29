7985df2dab5963be014fcfe49118e313e7ff50ad
Heng <tenn.luo@gmail.com>
2018-01-29 14:24:28 +0100

    upgrade ember to v2.8

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,7 +1,12 @@
 import EmberRouter from '@ember/routing/router';
 import config      from './config/environment';
 
-export default EmberRouter.extend({
+const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL:  config.rootURL
-}).map(function() {});
+});
+
+Router.map(function() {
+});
+
+export default Router;
