8732060a6c6058b2eb121aa14748b030fcb58d58
Pieter <Pieter901@hotmail.com>
2018-06-22 12:17:28 +0200

    Removed fuzzy search from auto-select (#63)

--- addon/components/select-auto-complete.js
+++ addon/components/select-auto-complete.js
@@ -9,40 +9,9 @@ export default Component.extend(formFieldOptions, {
   classNames: ['select-auto-complete'],
   layout,
 
-  _fuzzyMatch(haystack, needle) {
-    if (haystack == null || needle == null) {
-      return false;
-    }
-
-    haystack = haystack.toLowerCase();
-    needle   = needle.toLowerCase();
-
-    let n = -1;
-
-    for (let i = 0; i < needle.length; i++) {
-      let l = needle[i];
-      let m = n;
-      n = haystack.indexOf(l, n + 1);
-
-      if (n <= m) {
-        return false;
-      }
-    }
-
-    return true;
-  },
-
   actions: {
     onQueryChange(query) {
       this.sendAction('onQueryChange', query);
-
-      let content    = this.get('content');
-      let labelPath  = this.get('optionLabelPath');
-      let fuzzyMatch = this._fuzzyMatch;
-
-      if (query == null || content == null) { return content; }
-
-      return content.filter((item) => fuzzyMatch(item.get(labelPath), query));
     }
   }
 });
