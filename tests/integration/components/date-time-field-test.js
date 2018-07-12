4ace48b135aed3cd74c4cf5f3023f91366243ca1
Arne Diehl <arne.diehl@fabriquartz.com>
2018-07-12 15:31:16 +0200

    revert refactoring new test style

--- tests/integration/components/date-time-field-test.js
+++ tests/integration/components/date-time-field-test.js
@@ -1,57 +1,49 @@
 import hbs from 'htmlbars-inline-precompile';
 import run from 'ember-runloop';
 
-import { module, test } from 'qunit';
+import { moduleForComponent, test } from 'ember-qunit';
 
-import { setupRenderingTest } from 'ember-qunit';
-
-import { render, click } from '@ember/test-helpers';
-
-module('Integration | Component | {{date-time-field}}', function(hooks) {
-  setupRenderingTest(hooks);
-
-  hooks.beforeEach(function() {
-    this.actions = {};
-    this.send = (actionName, ...args) => this.actions[actionName].apply(this, args);
-  });
-
-  hooks.beforeEach(function() {
-    this.actions.update = function(value) {
+moduleForComponent(
+  'date-time-field', 'Integration | Component | {{date-time-field}}',
+{
+  integration: true,
+  beforeEach() {
+    this.on('update', function(value) {
       this.set('value', value);
-    };
-  });
-
-  test('shows date and time', async function(assert) {
-    this.set('value', new Date(2015, 0, 1, 12, 30));
-    await render(hbs`{{date-time-field value=value updated=(action "update")}}`);
-
-    let $dateInput = this.$('.date-picker');
-    let $timeInput = this.$('.time-field');
-
-    assert.equal($dateInput.val(), '01-01-15', 'shows date');
-    assert.equal($timeInput.val(), '12:30', 'shows time');
-
-    run(() => {
-      $dateInput.val('02-01-15');
-      $dateInput.trigger('input');
-      $timeInput.val('13:30');
-      $timeInput.trigger('input');
     });
+  }
+});
 
-    assert.equal(+this.get('value'), +(new Date(2015, 0, 2, 13, 30)),
-                 'updates date and time');
+test('shows date and time', function(assert) {
+  this.set('value', new Date(2015, 0, 1, 12, 30));
+  this.render(hbs`{{date-time-field value=value updated=(action "update")}}`);
+
+  let $dateInput = this.$('.date-picker');
+  let $timeInput = this.$('.time-field');
+
+  assert.equal($dateInput.val(), '01-01-15', 'shows date');
+  assert.equal($timeInput.val(), '12:30', 'shows time');
+
+  run(() => {
+    $dateInput.val('02-01-15');
+    $dateInput.trigger('input');
+    $timeInput.val('13:30');
+    $timeInput.trigger('input');
   });
 
-  test('set to now', async function(assert) {
-    await render(hbs`{{date-time-field value=value updated=(action "update")}}`);
-    await click('.date-time-field-clock');
-    assert.inDelta(+this.get('value'), +(new Date()), 1000);
-  });
+  assert.equal(+this.get('value'), +(new Date(2015, 0, 2, 13, 30)),
+               'updates date and time');
+});
 
-  test('unset time', async function(assert) {
-    this.set('value', new Date());
-    await render(hbs`{{date-time-field value=value updated=(action "update")}}`);
-    await click('.date-time-field-reset');
-    assert.equal(this.get('value'), null);
-  });
+test('set to now', function(assert) {
+  this.render(hbs`{{date-time-field value=value updated=(action "update")}}`);
+  this.$('.date-time-field-clock').click();
+  assert.inDelta(+this.get('value'), +(new Date()), 1000);
+});
+
+test('unset time', function(assert) {
+  this.set('value', new Date());
+  this.render(hbs`{{date-time-field value=value updated=(action "update")}}`);
+  this.$('.date-time-field-reset').click();
+  assert.equal(this.get('value'), null);
 });
--- tests/integration/components/text-field-test.js
+++ tests/integration/components/text-field-test.js
@@ -1,123 +1,116 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import run from 'ember-runloop';
 
-module('Integration | Component | {{text-field}}', function(hooks) {
-  setupRenderingTest(hooks);
+moduleForComponent('text-field', 'Integration | Component | {{text-field}}', {
+  integration: true
+});
 
-  hooks.beforeEach(function() {
-    this.actions = {};
-    this.send = (actionName, ...args) => this.actions[actionName].apply(this, args);
+test('renders input with placeholder', function(assert) {
+  this.render(hbs`{{text-field placeholder='Type your value here'}}`);
+
+  assert.equal(this.$('input')[0].getAttribute('placeholder'),
+               'Type your value here');
+});
+
+test('input value is set to value', function(assert) {
+  this.set('value', 'testing');
+  this.render(hbs`{{text-field value=value}}`);
+
+  assert.equal(this.$('input').val(), 'testing');
+});
+
+test('changing value changes input text', function(assert) {
+  this.set('value', 'testing');
+  this.render(hbs`{{text-field value=value}}`);
+
+  run(() => {
+    this.set('value', 'gnitset');
   });
 
-  test('renders input with placeholder', async function(assert) {
-    await render(hbs`{{text-field placeholder='Type your value here'}}`);
+  assert.equal(this.$('input').val(), 'gnitset');
+});
 
-    assert.equal(this.$('input')[0].getAttribute('placeholder'),
-                 'Type your value here');
+test(`typing doesn't change value but sends updated`, function(assert) {
+  assert.expect(2);
+
+  this.set('value', '');
+  this.on('update', function(value) {
+    assert.equal(value, 'x', 'calls update function with new value');
   });
 
-  test('input value is set to value', async function(assert) {
-    this.set('value', 'testing');
-    await render(hbs`{{text-field value=value}}`);
+  this.render(hbs`{{text-field value=value updated=(action "update")}}`);
 
-    assert.equal(find('input').value, 'testing');
+  let $input = this.$('input');
+
+  run(() => {
+    $input.val('x');
+    $input.trigger('input');
   });
 
-  test('changing value changes input text', async function(assert) {
-    this.set('value', 'testing');
-    await render(hbs`{{text-field value=value}}`);
+  assert.equal(this.get('value'), '');
+});
 
-    run(() => {
-      this.set('value', 'gnitset');
-    });
+test('leaving the input triggers change', function(assert) {
+  assert.expect(2);
 
-    assert.equal(find('input').value, 'gnitset');
+  this.set('value', '');
+  this.on('update', function(value) {
+    assert.equal(value, 'x', 'calls update function with new value');
   });
 
-  test(`typing doesn't change value but sends updated`, async function(assert) {
-    assert.expect(2);
+  this.render(hbs`{{text-field value=value updated=(action "update")}}`);
 
-    this.set('value', '');
-    this.actions.update = function(value) {
-      assert.equal(value, 'x', 'calls update function with new value');
-    };
+  let $input = this.$('input');
 
-    await render(hbs`{{text-field value=value updated=(action "update")}}`);
-
-    let $input = this.$('input');
-
-    run(() => {
-      $input.val('x');
-      $input.trigger('input');
-    });
-
-    assert.equal(this.get('value'), '');
+  run(() => {
+    $input.val('x');
+    $input.trigger('blur');
   });
 
-  test('leaving the input triggers change', async function(assert) {
-    assert.expect(2);
+  assert.equal(this.get('value'), '');
+});
 
-    this.set('value', '');
-    this.actions.update = function(value) {
-      assert.equal(value, 'x', 'calls update function with new value');
-    };
+test('"change" changes value', function(assert) {
+  assert.expect(2);
 
-    await render(hbs`{{text-field value=value updated=(action "update")}}`);
-
-    let $input = this.$('input');
-
-    run(() => {
-      $input.val('x');
-      $input.trigger('blur');
-    });
-
-    assert.equal(this.get('value'), '');
+  this.set('value', '');
+  this.on('update', function(value) {
+    assert.equal(value, 'x', 'calls update function with new value');
   });
 
-  test('"change" changes value', async function(assert) {
-    assert.expect(2);
+  this.render(hbs`{{text-field value=value updated=(action "update")}}`);
 
-    this.set('value', '');
-    this.actions.update = function(value) {
-      assert.equal(value, 'x', 'calls update function with new value');
-    };
+  let $input = this.$('input');
 
-    await render(hbs`{{text-field value=value updated=(action "update")}}`);
-
-    let $input = this.$('input');
-
-    run(() => {
-      $input.val('x');
-      $input.trigger('change');
-    });
-
-    assert.equal(this.get('value'), '');
+  run(() => {
+    $input.val('x');
+    $input.trigger('change');
   });
 
-  test('Format function only triggers when focusOut', async function(assert) {
-    this.set('value', '');
+  assert.equal(this.get('value'), '');
+});
 
-    this.set('format', (value) => {
-      assert.equal(value, '2', 'calls the format function with value');
-      return `${value}0`;
-    });
+test('Format function only triggers when focusOut', function(assert) {
+  this.set('value', '');
 
-    await render(hbs`{{text-field value=value format=format}}`);
-
-    let $input = this.$('input');
-
-    run(() => {
-      $input.val('2');
-    });
-
-    assert.equal($input.val(), '2', 'Input does not format on changing');
-
-    run(() => {
-      $input.trigger('blur');
-    });
-    assert.equal($input.val(), '20', 'FocusOut triggers format function');
+  this.set('format', (value) => {
+    assert.equal(value, '2', 'calls the format function with value');
+    return `${value}0`;
   });
+
+  this.render(hbs`{{text-field value=value format=format}}`);
+
+  let $input = this.$('input');
+
+  run(() => {
+    $input.val('2');
+  });
+
+  assert.equal($input.val(), '2', 'Input does not format on changing');
+
+  run(() => {
+    $input.trigger('blur');
+  });
+  assert.equal($input.val(), '20', 'FocusOut triggers format function');
 });
