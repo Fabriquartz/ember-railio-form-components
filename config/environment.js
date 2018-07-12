cc50657754f4517aabda051fee2daf08ffc2e580
Arne Diehl <arne.diehl@fabriquartz.com>
2018-07-12 16:13:09 +0200

    add ember-paper

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,16 @@
 /* eslint-env node */
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function(environment) {
+  let ENV = {
+    contentSecurityPolicy: {
+
+        // Allow fonts to be loaded from http://fonts.gstatic.com
+        'font-src': ["'self'", "http://fonts.gstatic.com"],
+
+        // Allow CSS loaded from https://fonts.googleapis.com
+        'style-src': ["'self'", "https://fonts.googleapis.com"],
+    }
+  }
+  return ENV;
 };
