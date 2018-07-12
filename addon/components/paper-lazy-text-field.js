c3253f99b4793f0675cbffb5e6c09d2973c1aa8b
Arne Diehl <arne.diehl@fabriquartz.com>
2018-07-12 13:42:02 +0200

    implement lazy paper text input

---
+++ addon/components/paper-lazy-text-field.js
@@ -0,0 +1,34 @@
+import { run }              from '@ember/runloop';
+import { action } from 'ember-decorators/object';
+import PaperTextField       from 'ember-railio-form-components/components/paper-text-field';
+import get                  from 'ember-metal/get';
+import set                  from 'ember-metal/set';
+
+export default PaperTextField.extend({
+  layoutName: 'components/paper-text-field',
+
+  focusIn() {
+    set(this, 'isFocused', true);
+  },
+
+  focusOut() {
+    set(this, 'isFocused', false);
+    this._super(...arguments);
+  },
+
+  withLazyDisabled(callback) {
+    let originalFocus = get(this, 'isFocused');
+    set(this, 'isFocused', false);
+
+    callback.call(this);
+
+    run.next(() => set(this, 'isFocused', originalFocus));
+  },
+
+  @action
+  changed(value) {
+    if (!get(this, 'isFocused')) {
+      this._super(...arguments);
+    }
+  }
+});
