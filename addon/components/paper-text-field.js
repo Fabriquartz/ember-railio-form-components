bbb429d7d60c947a2911d7fb370f4e53c066ae09
adiehl96 <a.diehl@student.ru.nl>
2018-08-08 15:35:48 +0200

    Ember Paper: Implement text input (#67)
    * add ember-paper
    
    * remove decorator dependency
    
    * Updated tests to work with ember-power-select
    
    * revert refactoring new test style
    
    * remove decorator dependency
    
    * create paper input Test
    
    * implement paper-text-field
    
    * refactor according to comments
    
    * refactor according to comments
    
    * refactored according to comments
    
    * Changes after feedback

---
+++ addon/components/paper-text-field.js
@@ -0,0 +1,68 @@
+import Component        from 'ember-component';
+import formFieldOptions from
+  'ember-railio-form-components/mixins/form-field-options';
+import { get, set } from '@ember/object';
+
+export default Component.extend(formFieldOptions, {
+  focusOut() {
+    let _value = get(this, '_value');
+    if (typeof this.attrs.focusOut === 'function') {
+      this.attrs.focusOut();
+    }
+
+    if (typeof get(this, 'format') === 'function') {
+      _value = this.format(_value);
+      set(this, '_value', _value);
+    }
+
+    this.send('changed', _value);
+  },
+
+  keyUp(e) {
+    if (typeof this.attrs.keyUp === 'function') {
+      this.attrs.keyUp();
+    }
+    if (e.keyCode === 13) {
+      if (typeof this.attrs.enter === 'function') {
+        this.attrs.enter();
+      }
+    }
+
+    if (e.keyCode === 27) {
+      if (typeof this.attrs.escape === 'function') {
+        this.attrs.escape();
+      }
+    }
+  },
+
+  didReceiveAttrs() {
+    let value = this.getAttr('value');
+    if (typeof this.formatValue === 'function') {
+      value = this.formatValue(value);
+    }
+    set(this, '_value', value);
+  },
+
+  actions: {
+    handleChanged(_value) {
+      set(this, '_value', _value);
+      if (typeof this.sanitizeValue === 'function') {
+        let originalValue = _value;
+
+        _value = this.sanitizeValue(_value);
+
+        if (originalValue !== _value) {
+          set(this, '_value', _value);
+        }
+      }
+
+      this.send('changed', _value);
+    },
+
+    changed(value) {
+      if (typeof this.attrs.updated === 'function') {
+        this.attrs.updated(value);
+      }
+    }
+  }
+});
---
+++ tests/integration/components/paper-text-field-test.js
@@ -0,0 +1,92 @@
+import { module, test }         from 'qunit';
+import { setupRenderingTest }   from 'ember-qunit';
+import { render, find, blur,
+  triggerEvent, focus, fillIn } from '@ember/test-helpers';
+import hbs                      from 'htmlbars-inline-precompile';
+
+module('Integration | Component | {{paper-text-field}}', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    this.actions = { updated: () => {} };
+  });
+
+  test('renders input with placeholder', async function(assert) {
+    await render(hbs`{{paper-text-field placeholder='Type your value here'}}`);
+
+    assert.equal(find('input').getAttribute('placeholder'),
+                 'Type your value here');
+  });
+
+  test('input value is set to value', async function(assert) {
+    this.set('value', 'testing');
+    await render(hbs`{{paper-text-field value=value}}`);
+
+    assert.equal(find('input').value, 'testing');
+  });
+
+  test('changing value changes input text', async function(assert) {
+    this.set('value', 'testing');
+    await render(hbs`{{paper-text-field value=value}}`);
+
+    await this.set('value', 'gnitset');
+
+    assert.equal(find('input').value, 'gnitset');
+  });
+
+  test(`typing doesn't change value but sends updated`, async function(assert) {
+    assert.expect(2);
+
+    this.set('value', '');
+    this.actions.update = function(value) {
+      assert.equal(value, 'x', 'calls update function with new value');
+    };
+
+    await render(hbs`{{paper-text-field value=value updated=(action "update")}}`);
+
+    let $input = find('input');
+    await fillIn($input, 'x');
+
+    assert.equal(this.get('value'), '');
+  });
+
+  test('"input" changes value', async function(assert) {
+    assert.expect(2);
+
+    this.set('value', '');
+    this.actions.update = function(value) {
+      assert.equal(value, 'x', 'calls update function with new value');
+    };
+
+    await render(hbs`{{paper-text-field value=value updated=(action "update")}}`);
+
+    let $input = find('input');
+
+    $input.value = 'x';
+    await triggerEvent($input, 'input');
+
+    assert.equal(this.get('value'), '');
+  });
+
+  test('Format function only triggers when focusOut', async function(assert) {
+    this.set('value', '');
+
+    this.set('format', (value) => {
+      assert.equal(value, '2', 'calls the format function with value');
+      return `${value}0`;
+    });
+
+    await render(hbs`{{paper-text-field value=value format=format}}`);
+
+    let $input = find('input');
+    await focus($input);
+
+    $input.value = '2';
+    await triggerEvent($input, 'input');
+
+    assert.equal($input.value, '2', 'Input does not format on changing');
+
+    await blur($input);
+    assert.equal($input.value, '20', 'FocusOut triggers format function');
+  });
+});
