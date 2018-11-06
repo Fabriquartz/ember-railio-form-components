ce9722194bc11f5a28e9035743df99fe0c8a82a0
Michael Wenting <michael.wenting@fabriquartz.com>
2018-11-06 14:41:48 +0100

    Moved, renamed and refactored text-field

---
+++ addon/components/paper/text-field.js
@@ -0,0 +1,62 @@
+import Component        from 'ember-component';
+import formFieldOptions from
+  'ember-railio-form-components/mixins/form-field-options';
+import { computed, get, set } from '@ember/object';
+
+export default Component.extend(formFieldOptions, {
+  lazy: false,
+
+  focusIn() {
+    set(this, 'isFocused', true);
+  },
+
+  focusOut() {
+    let value = this.format(get(this, '_value'));
+
+    set(this, 'isFocused', false);
+    set(this, '_value', value);
+    this.send('changed', value);
+
+    this._super(...arguments);
+  },
+
+  keyUp(e) {
+    if (e.keyCode === 13) { typeof this.enter  === 'function' && this.enter(); }
+    if (e.keyCode === 27) { typeof this.escape === 'function' && this.escape(); }
+  },
+
+  didReceiveAttrs() {
+    let value = get(this, 'value');
+    set(this, '_value', this.format(value));
+    this._super(...arguments);
+  },
+
+  format(value) {
+    return value;
+  },
+
+  _htmlAttributes: computed('htmlAttributes', 'name', function() {
+    let name           = get(this, 'name');
+    let options        = get(this, 'options') || {};
+    let htmlAttributes = get(options, 'htmlAttributes') || {};
+
+    if (!htmlAttributes.name && name) {
+      htmlAttributes.name = name;
+    }
+    return htmlAttributes;
+  }),
+
+  actions: {
+    changed(value) {
+      set(this, '_value', value);
+
+      if (!get(this, 'lazy') || !get(this, 'isFocused')) {
+        this.updated(value);
+      }
+    },
+
+    changedWetherLazyOrNot(value) {
+      this.updated(value);
+    }
+  }
+});
