c0d961be7ec558b4a922d59bef68801ecd0082c3
Pieter van Laatum <pieter.van.laatum@fabriquartz.com>
2019-02-01 14:44:35 +0100

    added file input

---
+++ addon/components/file-input.js
@@ -0,0 +1,56 @@
+import { A } from '@ember/array';
+import Component from '@ember/component';
+import { get, set, computed } from '@ember/object';
+import invokeAction from 'ember-invoke-action';
+import formFieldOptions from '../mixins/form-field-options';
+import layout from '../templates/components/file-input';
+
+export default Component.extend(formFieldOptions, {
+  layout,
+
+  classNames: ['file-input'],
+
+  type:          'file',
+  multiple:      false,
+  iconComponent: 'fa-icon',
+  iconName:      'upload',
+
+  fileCount: 0,
+
+  accept: computed('type', function() {
+    let type = get(this, 'type');
+    if (['image', 'video', 'audio'].includes(type)) {
+      return `${type}/*`;
+    }
+    return '*/*';
+  }),
+
+  actions: {
+    triggerInput() {
+      this.element.querySelector('input').click();
+    },
+
+    clear() {
+      let input = this.element.querySelector('input');
+      input.value = null;
+      input.dispatchEvent(new Event('change', { bubbles: true }));
+      // this.send('change');
+    },
+
+    change() {
+      let button   = this.element.querySelector('button');
+      let fileList = this.element.querySelector('input').files;
+      let files    = A();
+
+      button.blur();
+
+      for (let file of fileList) {
+        files.pushObject(file);
+      }
+
+      set(this, 'fileNames', files.mapBy('name'));
+      set(this, 'fileCount', files.length);
+      invokeAction(this, 'updated', files);
+    }
+  }
+});
---
+++ tests/integration/components/file-input-test.js
@@ -0,0 +1,138 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | {{file-input}}', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`{{file-input}}`);
+    assert.ok(find('button'), 'shows a button');
+    assert.ok(find('button > i.fa.fa-upload'), 'shows default icon in button');
+    assert.ok(find('input[type=file]'), 'Has (hidden) file input');
+    assert.equal(this.element.textContent.trim(), 'No file selected',
+      'shows default message');
+  });
+
+  test('sets `accept` attribute on input', async function(assert) {
+    await render(hbs`{{file-input}}`);
+
+    let $input = find('input');
+    assert.equal($input.getAttribute('accept'), '*/*',
+      'accept all files by default');
+
+    this.set('type', 'foobar');
+    await render(hbs`{{file-input type=type}}`);
+    $input = find('input');
+
+    assert.equal($input.getAttribute('accept'), '*/*',
+      'accept all files by when type is not a regular file type');
+
+    this.set('type', 'image');
+    assert.equal($input.getAttribute('accept'), 'image/*',
+      'accept all image files by when type is image');
+
+    this.set('type', 'video');
+    assert.equal($input.getAttribute('accept'), 'video/*',
+      'accept all video files by when type is video');
+
+    this.set('type', 'audio');
+    assert.equal($input.getAttribute('accept'), 'audio/*',
+      'accept all audio files by when type is audio');
+
+    this.set('accept', '.foo');
+    await render(hbs`{{file-input accept=accept}}`);
+    $input = find('input');
+    assert.equal($input.getAttribute('accept'), '.foo',
+      'accept attribut is overridden by component attribute');
+  });
+
+  test('shows `clear` button', async function(assert) {
+    this.set('fileCount', 0);
+    await render(hbs`{{file-input fileCount=fileCount}}`);
+    assert.notOk(find('.file-input__clear'),
+      'shows no `clear` button when no files are selected');
+
+    this.set('fileCount', 1);
+    assert.ok(find('.file-input__clear'),
+      'shows `clear` button when files are selected');
+  });
+
+  test('shows file names', async function(assert) {
+    this.set('fileNames', ['foo.bar', 'baz.bar']);
+    this.set('fileCount', 0);
+    await render(hbs`{{file-input fileCount=fileCount fileNames=fileNames}}`);
+
+    assert.notOk(find('.file-input__file-names'),
+     'shows no file names when no files are selected');
+
+    this.set('fileCount', 1);
+
+    let $fileNames = find('.file-input__file-names').querySelectorAll('li');
+
+    assert.equal($fileNames.length, 2, 'list of file names shows 2 items');
+    assert.equal($fileNames[0].textContent.trim(), 'foo.bar',
+      'shows first file name');
+    assert.equal($fileNames[1].textContent.trim(), 'baz.bar',
+      'shows second file name');
+  });
+
+  test('shows number of files', async function(assert) {
+    this.set('fileCount', 0);
+
+    await render(hbs`{{file-input fileCount=fileCount multiple=true}}`);
+    let $fileCount = find('.file-input__file-count');
+
+    assert.notOk($fileCount, 'shows no count when no files are selected');
+
+    this.set('fileCount', 1);
+    $fileCount = find('.file-input__file-count');
+    assert.equal($fileCount.textContent.trim(), '1 file selected',
+      'Shows singular name when one file is selected');
+
+    this.set('fileCount', 2);
+    assert.equal($fileCount.textContent.trim(), '2 files selected',
+      'Shows plural name when multiple files are selected');
+  });
+
+  test('labels are showing given type', async function(assert) {
+    this.set('fileCount', 0);
+    this.set('multiple', false);
+    this.set('type', 'image');
+
+    await render(hbs`{{file-input fileCount=fileCount
+                                  multiple=multiple
+                                  type=type}}`);
+
+    assert.equal(find('.file-input__no-files').textContent.trim(),
+      'No image selected',
+      'Empty input label shows given type (multiple=false)');
+
+    this.set('fileCount', 1);
+
+    assert.equal(find('.file-input__file-count').textContent.trim(),
+      'image selected',
+      'Selected files label shows given type (multiple=false)');
+
+    this.set('fileCount', 0);
+    this.set('multiple', true);
+
+    assert.equal(find('.file-input__no-files').textContent.trim(),
+      'No images selected',
+      'Empty input label shows given type (multiple=true)');
+
+    this.set('fileCount', 1);
+
+    assert.equal(find('.file-input__file-count').textContent.trim(),
+      '1 image selected',
+      'Selected files label shows given type singularized (multiple=true)');
+
+    this.set('fileCount', 2);
+
+    assert.equal(find('.file-input__file-count').textContent.trim(),
+      '2 images selected',
+      'Selected files label shows given type pluralized (multiple=true)');
+
+  });
+});
