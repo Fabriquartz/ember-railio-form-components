12d652e63ad779d314ebac43388f808076d05ae1
Pieter <Pieter901@hotmail.com>
2019-05-09 13:54:28 +0200

    Upgrade Ember to version 3.9 (#92)

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,19 +13,6 @@ module.exports = function() {
       useYarn:   true,
       scenarios: [
         {
-          name: 'ember-lts-2.16',
-          env:  {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.6.0',
-              'ember-source':  '~2.16.0'
-            }
-          },
-          bower: {}
-        },
-        {
           name: 'ember-lts-2.18',
           env:  {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
@@ -40,6 +27,19 @@ module.exports = function() {
         },
         {
           name: 'ember-lts-3.4',
+          env:  {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
+          },
+          npm: {
+            devDependencies: {
+              '@ember/jquery': '^0.6.0',
+              'ember-source':  '~3.4.0'
+            }
+          },
+          bower: {}
+        },
+        {
+          name: 'ember-lts-3.8',
           env:  {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
           },
